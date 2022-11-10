a48f719db44e5f41907e0fecb897ce09e3366e3b
Allan Oliveira <allandavidev@gmail.com>
2022-11-10 19:21:56 -0300

    Feat: Implementando navegação entre paginas

--- src/components/ButtonHome/index.js
+++ src/components/ButtonHome/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-export default function ButtonHome() {
+export default function ButtonHome(navigation) {
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate('');
+        }}>
         <FontAwesome5 name={'calendar'} size={20} color={'white'} />
         <Text style={styles.textButton}>CALLENDAR</Text>
       </TouchableOpacity>
